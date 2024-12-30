23c4cab15a377980ee373369503e57d20a09256f
Bipin Khatri <159134672+Bipinkhatree@users.noreply.github.com>
2024-12-30 17:17:23 +0545

    Finish the html layout basic css and javascript logic(Forgot to commit early)

---
+++ script.js
@@ -0,0 +1,96 @@
+let number1;
+let number2;
+let op;
+
+let numbers = document.querySelectorAll(".number");
+
+let operators = document.querySelectorAll(".operator");
+
+let clear = document.querySelector(".clear");
+
+let input = document.querySelector("#input");
+
+let result = document.querySelector(".result");
+
+let backspace = document.querySelector(".backspace")
+
+
+
+backspace.addEventListener('click', () => {
+    input.value = input.value.slice(0, -1);
+})
+clear.addEventListener('click', () => {
+    input.value = '';
+    number1 = ''
+    number2 = ''
+    op = ''
+})
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function division(num1, num2) {
+    if (num2 == 0) {
+        return "Error: Division by Zero";         //Handle division by zero
+    }
+    return num1 / num2;
+}
+numbers.forEach((number) => {
+    number.addEventListener('click', (e) => {
+        input.value += e.target.value;        //Append the clicked number into input
+    });
+});
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (e) => {
+        if (input.value === "") {
+            return;      // Prevent operator selection without input value
+        }
+        number1 = parseInt(input.value); //parse the first number
+        op = e.target.value;   //store the operator
+        input.value = " ";     //clear the input for the second value
+    });
+});
+
+result.addEventListener('click', () => {
+    if (input.value === "") {
+        return; //prevent calculation if there's no second number entered
+    }
+    number2 = parseInt(input.value); //parse the second number
+    finalResult = operate(number1, number2, op); //perform the calculation
+
+    if (finalResult === "Error: Division by Zero" || finalResult === "Invalid Operator") {
+        input.value = ''; //clear the input after the error
+    } else {
+        input.value = finalResult;     //Display the result in the input field
+    }
+
+})
+
+function operate(number1, number2, operator) {
+
+    switch (operator) {
+        case '+':
+            return add(number1, number2);
+
+        case '-':
+            return sub(number1, number2);
+
+        case '*':
+            return multiply(number1, number2);
+
+        case '/':
+            return division(number1, number2);
+
+        default:
+            return "Invalid Operator"      //Handle invalid operator
+    }
+}
